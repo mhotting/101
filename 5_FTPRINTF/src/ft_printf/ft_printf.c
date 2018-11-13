805d0fc0dfd7dfba88f9a09250bdbbfdebf2a055
mhotting <madric.hottinger.101@gmail.com>
2018-11-13 18:52:20 +0100

    Restart printf

--- 5_FTPRINTF/src/ft_printf/ft_printf.c
+++ 5_FTPRINTF/src/ft_printf/ft_printf.c
@@ -1,38 +1,49 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_printf.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mhotting <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/11/13 14:02:32 by mhotting     #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/13 18:51:42 by mhotting    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 #include "./../../includes/libft.h"
 #include <stdio.h>
 
-/* EXTRACT FROM STRING */
-size_t	ft_endformat(char *format, size_t i)
+static void	ft_dispatch(char *str, size_t i, va_list *ap)
 {
-	size_t	j;
+	int	x;
 
-	j = 0;
-	return (j);
-}
-
-/* PRINTF FUNCTIONS */
-
-void	ft_putarg(va_list ap, char *format, size_t i)
-{
-	return ;
+	x = va_arg(*ap, int);
+	if (str[i] == '%')
+		printf("L'entier donne est: %d\n", x);
 }
 
 int		ft_printf(const char *format, ...)
 {
+	char	*str;
 	va_list	ap;
+	int		len_ret;
 	size_t	i;
 
 	if (format == NULL)
 		return (0);
+	str = ft_strdup(format);
 	va_start(ap, format);
 	i = 0;
-	while (format[i])
+	while (str[i] != '\0')
 	{
-		if (format[i] == '%')
-			ft_putarg(ap, format, i);
+		if (str[i] == '%')
+			ft_dispatch(str, i, &ap);
 		i++;
 	}
-	ft_putstr(format);
+	ft_putstr(str);
 	va_end(ap);
-	return (ft_strlen(format));
+	len_ret = (int)ft_strlen(str);
+	free(str);
+	return (len_ret);
 }
