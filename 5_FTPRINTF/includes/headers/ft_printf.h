5100dde80262a0d4cf6f91683e419141d2e18a68
mhotting <madric.hottinger.101@gmail.com>
2018-11-20 19:32:04 +0100

    Renamed printf

---
+++ 5_FTPRINTF/includes/headers/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_printf.h                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mhotting <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/11/20 17:54:35 by mhotting     #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/20 18:53:41 by mhotting    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+
+typedef			char *(*pf_func)(char *, va_list *);
+typedef struct	s_conf
+{
+	char	*str;
+	pf_func	func;
+}				t_conv;
+
+int				ft_printf(const char *format, ...);
+
+#endif
---
+++ 5_FTPRINTF/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mhotting <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/11/13 14:11:21 by mhotting     #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/20 18:57:18 by mhotting    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "./includes/libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+<<<<<<< HEAD
+	printf("NB1: %d\n", 3);
+	ft_printf("Bonjour a tous les %d.\n", 5);
+=======
+	printf("NB1: %u\n", -3);
+	ft_printf("Bonjour a tous les %d.\n", -3);
+>>>>>>> a5dbf4ebf72d86197b3275c148ed9ba0d40a65c0
+	return (0);
+}
