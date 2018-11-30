489a25ecbd0cbd3bd37b4ced4bce6e711dd97b56
mhotting <madric.hottinger.101@gmail.com>
2018-11-30 17:01:46 +0100

    Added godd v1 + norme

--- 5_FTPRINTF/src/ft_printf/pf_func/pf_c_arg.c
+++ 5_FTPRINTF/src/ft_printf/pf_func/pf_c_arg.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/23 11:29:21 by mhotting     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/30 16:53:51 by mhotting    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/30 16:59:26 by mhotting    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,13 +19,15 @@ static void	ft_applyflag1(char **res, t_attributes *att, char c)
 	if (att->width != -1)
 	{
 		if (att->opt1 == 1)
-			ft_enhance_right(res, ' ', (c == '\0' ? att->width + ft_strlen("@@+NULL+@@") - 1 : att->width));
+			ft_enhance_right(res, ' ', (c == '\0' ? att->width +
+						ft_strlen("@@+NULL+@@") - 1 : att->width));
 		else
-			ft_enhance_left(res, ' ', (c == '\0' ? att->width + ft_strlen("@@+NULL+@@") - 1 : att->width));
+			ft_enhance_left(res, ' ', (c == '\0' ? att->width +
+						ft_strlen("@@+NULL+@@") - 1 : att->width));
 	}
 }
 
-char	*pf_c_arg(char *sub, va_list *ap, t_attributes *att)
+char		*pf_c_arg(char *sub, va_list *ap, t_attributes *att)
 {
 	char	*res;
 	char	c;
