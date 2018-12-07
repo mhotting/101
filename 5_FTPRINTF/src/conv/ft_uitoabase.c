22632fceb08f6a6b6673f3084ff51a98b5de79a5
mhotting <madric.hottinger.101@gmail.com>
2018-12-07 09:53:12 +0100

    Added missing files

---
+++ 5_FTPRINTF/src/conv/ft_uitoabase.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_ullitoabase.c                                 .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mhotting <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/11/27 17:27:27 by mhotting     #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/27 22:00:31 by mhotting    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "./../../includes/libft.h"
+
+static void	ft_convert(unsigned long long int n, int base, char *ref, char *res)
+{
+	if (n / base == 0)
+	{
+		res[ft_strlen(res)] = ref[n % base];
+		return ;
+	}
+	ft_convert((n / base), base, ref, res);
+	res[ft_strlen(res)] = ref[n % base];
+}
+
+char		*ft_uitoabase(unsigned long long int n, int base)
+{
+	char	*res;
+	char	*temp;
+	char	*ref;
+
+	if (base == 10)
+		return (ft_itoa(n));
+	ref = ft_strdup("0123456789abcdefghijklmnopqrstuvwxyz");
+	if (base < 2 || base > 30)
+		return (NULL);
+	res = ft_strnew(65);
+	if (res == NULL)
+		return (NULL);
+	ft_convert(n, base, ref, res);
+	temp = res;
+	res = ft_strdup(res);
+	free(ref);
+	free(temp);
+	return (res);
+}
---
+++ 5_FTPRINTF/src/ft_printf/util.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   util.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mhotting <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/11/29 09:34:09 by mhotting     #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/30 18:09:36 by mhotting    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "./../../includes/libft.h"
+#include <stdio.h>
+
+size_t	ft_putstr_pf(char *str)
+{
+	char	*add;
+	int		cpt;
+
+	cpt = 0;
+	add = ft_strstr(str, N);
+	if (add == NULL)
+		ft_putstr(str);
+	else
+	{
+		cpt++;
+		while (str < add)
+		{
+			ft_putchar(*str);
+			str++;
+		}
+		ft_putchar('\0');
+		cpt += ft_putstr_pf(str + ft_strlen(N));
+	}
+	return (cpt);
+}
+
+void	ft_delzero(char **res)
+{
+	char	*temp;
+
+	temp = ft_strnew(0);
+	free(*res);
+	*res = temp;
+}
+
+void	ft_enhance_left(char **res, char c, int len)
+{
+	char	*temp;
+	int		old_len;
+	int		i;
+	int		j;
+
+	old_len = (int)ft_strlen(*res);
+	if (old_len >= len)
+		return ;
+	temp = ft_strnew(len);
+	if (temp == NULL)
+		return ;
+	i = 0;
+	while (i < (len - old_len))
+		temp[i++] = c;
+	j = 0;
+	while (i < len)
+		temp[i++] = (*res)[j++];
+	free(*res);
+	*res = temp;
+}
+
+void	ft_enhance_right(char **res, char c, int len)
+{
+	char	*temp;
+	int		old_len;
+	int		i;
+
+	old_len = (int)ft_strlen(*res);
+	if (old_len >= len)
+		return ;
+	temp = ft_strnew(len);
+	if (temp == NULL)
+		return ;
+	i = -1;
+	while (++i < old_len)
+		temp[i] = (*res)[i];
+	while (i < len)
+		temp[i++] = c;
+	free(*res);
+	*res = temp;
+}
+
+void	ft_intadjust(char *res, t_attributes *att)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	c;
+
+	len = (int)ft_strlen(res);
+	i = 0;
+	while (res[i] == ' ')
+		i++;
+	if (res[i] != '0' || (res[i] == '0' && att->opt1 == 1))
+		return ;
+	j = i;
+	while (j < len && res[j] != '+' && res[j] != '-' &&
+			res[j] != 'x' && res[j] != 'X' && res[j] != ' ')
+		j++;
+	if (j == len)
+		return ;
+	if (res[j] == 'x' || res[j] == 'X')
+		i++;
+	c = res[i];
+	res[i] = res[j];
+	res[j] = c;
+	return ;
+}
