06a6a7330cc21b1d885c50e9879b0de27663d899
mhotting <madric.hottinger.101@gmail.com>
2018-12-11 18:40:40 +0100

    Printf v1

--- 5_FTPRINTF/src/conv/ft_ldtoa.c
+++ 5_FTPRINTF/src/conv/ft_ldtoa.c
@@ -6,12 +6,63 @@
 /*   By: mhotting <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/25 10:07:50 by mhotting     #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/10 16:06:50 by mhotting    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/12/11 18:37:43 by mhotting    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "./../../includes/libft.h"
+#include <stdio.h>
+
+static void	ft_extra_digit(char **res)
+{
+	char	*temp;
+	size_t	len;
+	int		i;
+
+	len = ft_strlen(*res);
+	temp = ft_strnew(len + 1);
+	if (temp == NULL)
+		return ;
+	ft_strcpy(temp, *res);
+	i = (int)len;
+	while (i > 0)
+	{
+		temp[i] = temp[i - 1];
+		i--;
+	}
+	if (temp[0] == '-')
+		temp[1] = '1';
+	else
+		temp[0] = '1';
+	free(*res);
+	*res = temp;
+}
+
+static void	ft_rounder(char **res, int num, int index)
+{
+	if (num < 5)
+		return ;
+	if ((*res)[index] == '.')
+		ft_rounder(res, num, index - 1);
+	if ((*res)[index] == '9' &&
+			(index == 0 || ((*res)[0] == '-' && index == 1)))
+	{
+		(*res)[index] = '0';
+		ft_extra_digit(res);
+		return ;
+	}
+	if ((*res)[index] < '9')
+	{
+		(*res)[index] += 1;
+		return ;
+	}
+	else
+	{
+		(*res)[index] = '0';
+		ft_rounder(res, 6, index - 1);
+	}
+}
 
 static void	ft_neg(int *index, long double *f, char *res)
 {
@@ -32,7 +83,7 @@ static void	ft_decimal(char *res, int prec, long double *f, int *index)
 		int_part = (int)(*f);
 		res[index[0]] = int_part + '0';
 		index[0]++;
-		*f = *f - int_part;
+		*f = *f - (long double)int_part;
 		d++;
 	}
 }
@@ -56,10 +107,9 @@ char		*ft_ldtoa(long double f, int prec)
 	if (prec > 0)
 		res[index[0]++] = '.';
 	free(temp);
-	f -= index[2];
+	f -= (long double)index[2];
 	ft_decimal(res, prec, &f, index);
-	if ((int)(f * 10) >= 5)
-		res[index[0] - 1]++;
+	ft_rounder(&res, (int)(f * 10), index[0] - 1);
 	temp = res;
 	res = ft_strdup(res);
 	free(temp);
